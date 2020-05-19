0e7a6fef536b91f6d23cc37aeb809b8eb9429675
Simba_cheng <simba_cheng@163.com>
2020-05-20 00:06:42 +0800

    剔除 lombok

--- Farmer-SpringBoot/src/main/java/com/server/vo/response/ResExitNodePathVO.java
+++ Farmer-SpringBoot/src/main/java/com/server/vo/response/ResExitNodePathVO.java
@@ -1,12 +1,11 @@
 package com.server.vo.response;
 
-import lombok.Data;
 
 /**
  * @author CYX
  * @Date: 2018/11/15 11:37
  */
-@Data
+
 public class ResExitNodePathVO {
     /**
      * 操作zk-成功失败标识
@@ -38,6 +37,46 @@ public class ResExitNodePathVO {
      */
     private String isExist;
 
+    public String getIsSuccess() {
+        return isSuccess;
+    }
+
+    public void setIsSuccess(String isSuccess) {
+        this.isSuccess = isSuccess;
+    }
+
+    public ResErrorInfo getErrorInfo() {
+        return errorInfo;
+    }
+
+    public void setErrorInfo(ResErrorInfo errorInfo) {
+        this.errorInfo = errorInfo;
+    }
+
+    public String getDisplayCopy() {
+        return displayCopy;
+    }
+
+    public void setDisplayCopy(String displayCopy) {
+        this.displayCopy = displayCopy;
+    }
+
+    public String getNodePath() {
+        return nodePath;
+    }
+
+    public void setNodePath(String nodePath) {
+        this.nodePath = nodePath;
+    }
+
+    public String getIsExist() {
+        return isExist;
+    }
+
+    public void setIsExist(String isExist) {
+        this.isExist = isExist;
+    }
+
     public ResExitNodePathVO() {
     }
 
@@ -47,5 +86,17 @@ public class ResExitNodePathVO {
         this.displayCopy = displayCopy;
         this.nodePath = nodePath;
         this.isExist = isExist;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ResExitNodePathVO{");
+        sb.append("isSuccess='").append(isSuccess).append('\'');
+        sb.append(", errorInfo=").append(errorInfo);
+        sb.append(", displayCopy='").append(displayCopy).append('\'');
+        sb.append(", nodePath='").append(nodePath).append('\'');
+        sb.append(", isExist='").append(isExist).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }

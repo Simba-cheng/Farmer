2dee4b79304308e58ac14dc4d3164a42d1b621e7
Simba_cheng <simba_cheng@163.com>
2020-05-20 00:06:18 +0800

    剔除 lombok

--- Farmer-SpringBoot/src/main/java/com/server/vo/response/ResCreateAllNodeVO.java
+++ Farmer-SpringBoot/src/main/java/com/server/vo/response/ResCreateAllNodeVO.java
@@ -1,12 +1,11 @@
 package com.server.vo.response;
 
-import lombok.Data;
 
 /**
  * @author CYX
  * @date: 2018/12/15 10:15
  */
-@Data
+
 public class ResCreateAllNodeVO {
 
     /**
@@ -32,6 +31,38 @@ public class ResCreateAllNodeVO {
      */
     private String nodePath;
 
+    public String getIsSuccess() {
+        return isSuccess;
+    }
+
+    public void setIsSuccess(String isSuccess) {
+        this.isSuccess = isSuccess;
+    }
+
+    public ResErrorInfo getErrorInfo() {
+        return errorInfo;
+    }
+
+    public void setErrorInfo(ResErrorInfo errorInfo) {
+        this.errorInfo = errorInfo;
+    }
+
+    public String getDisplayCopy() {
+        return displayCopy;
+    }
+
+    public void setDisplayCopy(String displayCopy) {
+        this.displayCopy = displayCopy;
+    }
+
+    public String getNodePath() {
+        return nodePath;
+    }
+
+    public void setNodePath(String nodePath) {
+        this.nodePath = nodePath;
+    }
+
     public ResCreateAllNodeVO() {
     }
 
@@ -40,5 +71,16 @@ public class ResCreateAllNodeVO {
         this.errorInfo = errorInfo;
         this.displayCopy = displayCopy;
         this.nodePath = nodePath;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ResCreateAllNodeVO{");
+        sb.append("isSuccess='").append(isSuccess).append('\'');
+        sb.append(", errorInfo=").append(errorInfo);
+        sb.append(", displayCopy='").append(displayCopy).append('\'');
+        sb.append(", nodePath='").append(nodePath).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }

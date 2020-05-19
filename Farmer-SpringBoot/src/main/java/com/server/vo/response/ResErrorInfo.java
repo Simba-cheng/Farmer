f41cfe1c0a7ee4cdd7a3932237df108b9d604984
Simba_cheng <simba_cheng@163.com>
2020-05-20 00:06:36 +0800

    剔除 lombok

--- Farmer-SpringBoot/src/main/java/com/server/vo/response/ResErrorInfo.java
+++ Farmer-SpringBoot/src/main/java/com/server/vo/response/ResErrorInfo.java
@@ -1,6 +1,5 @@
 package com.server.vo.response;
 
-import lombok.Data;
 
 /**
  * 错误信息
@@ -8,7 +7,7 @@ import lombok.Data;
  * @author CYX
  * @create 2018-11-19-23:33
  */
-@Data
+
 public class ResErrorInfo {
 
     /**
@@ -21,11 +20,36 @@ public class ResErrorInfo {
      */
     private String errorMessage;
 
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
     public ResErrorInfo() {
     }
 
     public ResErrorInfo(String errorCode, String errorMessage) {
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ResErrorInfo{");
+        sb.append("errorCode='").append(errorCode).append('\'');
+        sb.append(", errorMessage='").append(errorMessage).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
